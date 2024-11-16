7a23b821ab8c8d29a680b09dc3d2ff33ac826f72
Amit Pathak <amit.c.pathak@avanade.com>
2024-11-15 22:31:58 -0500

    Added Leetcode Pandas Tutorial

---
+++ LeetCode/Pandas/dataFrame.py
@@ -0,0 +1,26 @@
+import pandas as pd
+print("Hello")
+
+data = [
+  [1, 15],
+  [2, 11],
+  [3, 11],
+  [4, 20]
+]
+
+#Create a DataFrame from List
+df = pd.DataFrame(data, columns=['student_id', 'age'])
+#print(df)
+
+#Get the Size of a DataFrame
+#print(df.shape)
+
+#Display the First Three Rows
+#print(df.head(3))
+
+#Select Data
+#print(df[df['student_id'] == 4][[ 'age']])
+#print(df.loc[df['student_id']== 4, ['age']])
+
+#Create a New Column
+print(df.assign(double_age=df['age']*2))
